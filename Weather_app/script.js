3c7f24163a12cd2252fd68f5a70568c1063ffeb6
ShubhamKumar4894 <shubham.ydv4894@gmail.com>
2024-10-01 10:56:01 +0530

    temperature

--- Weather_app/script.js
+++ Weather_app/script.js
@@ -1,11 +1,14 @@
 const button= document.querySelector('.button');
 const Input= document.querySelector('.Input');
-const temperature=;
+const temp= document.querySelector('.temperature');
+const wind= document.querySelector('.desc');
 
 async function checkWeather(city){
     const apiKey="7fad108574dbbdc308807e719da0d223";
     const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const data= await fetch(`${url}`).then(Response => Response.json());
+    temp.innerHTML=`${Math.round(data.main.temp)}°C`;
+    console.log(data);
     
 }
 button.addEventListener('click',function(){
